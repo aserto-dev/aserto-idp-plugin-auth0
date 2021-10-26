fbf4a012b551d3f8a05310dd647f194c33bf9e33
oanatmaria <oana@aserto.com>
2021-10-26 17:20:44 +0300

    Add unit tests

---
+++ pkg/srv/test_utils.go
@@ -0,0 +1,52 @@
+package srv
+
+import (
+	"strings"
+	"time"
+
+	api "github.com/aserto-dev/go-grpc/aserto/api/v1"
+	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"gopkg.in/auth0.v5"
+	"gopkg.in/auth0.v5/management"
+)
+
+func CreateTestApiUser(id, displayName, email, picture string) *api.User {
+	user := api.User{
+		Id:          id,
+		DisplayName: displayName,
+		Email:       email,
+		Picture:     picture,
+		Identities:  make(map[string]*api.IdentitySource),
+		Attributes: &api.AttrSet{
+			Properties:  &structpb.Struct{Fields: make(map[string]*structpb.Value)},
+			Roles:       []string{},
+			Permissions: []string{},
+		},
+		Applications: make(map[string]*api.AttrSet),
+		Metadata: &api.Metadata{
+			CreatedAt: timestamppb.New(time.Now()),
+			UpdatedAt: timestamppb.New(time.Now()),
+		},
+	}
+
+	return &user
+}
+
+func CreateTestAuth0User(id, displayName, email, picture, phoneNo, userName string) *management.User {
+
+	metadata := make(map[string]interface{})
+	metadata["phoneNumber"] = phoneNo
+	metadata[strings.ToLower(api.IdentityKind_IDENTITY_KIND_PHONE.String())] = phoneNo
+	metadata[strings.ToLower(api.IdentityKind_IDENTITY_KIND_USERNAME.String())] = userName
+
+	user := management.User{
+		ID:           auth0.String(id),
+		Nickname:     auth0.String(displayName),
+		Email:        auth0.String(email),
+		Picture:      auth0.String(picture),
+		UserMetadata: metadata,
+	}
+
+	return &user
+}
